8e4b3d093a2a12540cf4cd5a0d480596493f0459
Jeff Gonzalez <jeff@hypertheory.com>
2023-03-27 16:19:27 -0400

    before records

---
+++ instructor/NewCSharpStuff/Customer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace NewCSharpStuff;
+
+public class Customer
+{
+    public string? Name { get; set; }
+
+    public int? Age { get; set; }
+
+    public List<Order> Orders { get; set; }
+
+}
+
+
+public class Order { }
---
+++ instructor/NewCSharpStuff/Program.cs
@@ -0,0 +1,27 @@
+﻿// Everything in here goes to static Void Main
+
+
+using NewCSharpStuff;
+
+Console.WriteLine("Hello, World!");
+
+
+var c1 = new Customer()
+{
+    Name = "Bob",
+
+   
+};
+c1.Orders.Add(new Order());
+
+if (c1.Name is not null && c1.Age.HasValue)
+{
+    Console.WriteLine($"The name is {c1.Name} and the age is {c1.Age.Value}");
+
+}
+else
+{
+}
+
+
+// Sir Tony Hoare "My Four Billion Dollar Mistake"
