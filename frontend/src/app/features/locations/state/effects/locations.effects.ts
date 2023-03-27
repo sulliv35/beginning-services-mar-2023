ba2ddc8a36f1fd624a2dfbd74c3073b53abddb58
Jeff Gonzalez <Jeff@hypertheory.com>
2023-03-27 13:00:03 -0400

    Updated Frontend

--- frontend/src/app/features/locations/state/effects/locations.effects.ts
+++ frontend/src/app/features/locations/state/effects/locations.effects.ts
@@ -1,16 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, switchMap } from 'rxjs';
+import { map, mergeMap, switchMap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { LocationEntity } from '..';
 import {
   locationCommands,
   locationDocuments,
+  locationEvents,
 } from '../actions/location.actions';
+
 @Injectable()
 export class LocationEffects {
   private readonly baseUrl = environment.locationsApi + 'locations';
+
+  saveLocation$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(locationEvents.locationCreated),
+      mergeMap(({ payload }) =>
+        this.client
+          .post<LocationEntity>(this.baseUrl, payload)
+          .pipe(map((payload) => locationDocuments.location({ payload })))
+      )
+    );
+  });
 
   loadLocations$ = createEffect(() => {
     return this.actions$.pipe(
--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,7 +1,42 @@
 import { setupWorker, rest } from 'msw';
-
+import * as cuid from 'cuid';
+const url = 'http://localhost:1337/';
 export const mocks = [
-  rest.get('http://localhost:1337/locations', (req, res, ctx) => {
+
+  rest.get(url + 'support', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        "contactInfo": {
+          "name": "Andrew Mervkin",
+          "phone": "888-1212",
+          "email": "merv@pumpkin.com"
+        },
+        uptime: {
+          days: 13,
+          hours: 12,
+          minutes: 38
+        }
+      })
+    )
+  }),
+
+  rest.post(url + 'locations', async (req, res, ctx) => {
+    const reqBody = await req.json();
+    const authHeader = (req.headers.get('Authorization')?.split('.')[1] || 'none');
+    const sub = JSON.parse(atob(authHeader)).sub;
+    return res(
+      ctx.status(201),
+      ctx.json({
+        id: cuid(),
+        addedBy: sub,
+        addedOn: new Date().toISOString(),
+        ...reqBody
+
+      })
+    )
+  }),
+  rest.get(url + 'locations', (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
