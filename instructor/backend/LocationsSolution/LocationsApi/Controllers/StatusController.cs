da0e3cc8c654b813692f8ae93f0f18fb34de9499
Jeff Gonzalez <jeff@hypertheory.com>
2023-03-27 15:07:44 -0400

    before break

---
+++ instructor/backend/LocationsSolution/LocationsApi/Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using LocationsApi.Models;
+using LocationsApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationsApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    private readonly UptimeClock _clock;
+
+    public StatusController(UptimeClock clock)
+    {
+        _clock = clock;
+    }
+
+
+    // GET /status
+    [HttpGet("/status")]
+    public async Task<ActionResult> GetStatus()
+    {
+        var response = new GetStatusResponse()
+        {
+               ContactInfo = new ContactInfo()
+               {
+                   Name = "Bob Smith",
+                   Email = "bob@aol.com",
+                   Phone = "555-1212"
+               },
+               Uptime = new Uptime
+               {
+                   Hours = 23,
+                   Minutes = 59,
+                   Days = 2384,
+               }
+        };
+        return Ok(response);
+    }
+}
--- instructor/backend/LocationsSolution/LocationsApi/Program.cs
+++ instructor/backend/LocationsSolution/LocationsApi/Program.cs
@@ -1,14 +1,22 @@
+using LocationsApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// this wouldd have in "ConfigureServices" in your Startup.cs
 // Add services to the container.
+// all the behind the scenes stuff that makes up our application
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var clock = new UptimeClock();
+builder.Services.AddSingleton<UptimeClock>(clock);
 
+var app = builder.Build();
+// This would have been in the Configure method in startup
+// the thing that will actually handle incoming requests.
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,5 +27,8 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+// Create the "Route Table" - phone book.
+// GET /status -> StatusController -> GetStatus
 
-app.Run();
+
+app.Run(); // Blocking - where the "kestrel web server starts listening"
