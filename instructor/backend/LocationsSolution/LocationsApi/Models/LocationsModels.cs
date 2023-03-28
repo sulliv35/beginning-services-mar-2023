a24f18f8958d83868b28fb721ef990922b3176b5
Jeff Gonzalez <jeff@hypertheory.com>
2023-03-28 16:50:10 -0400

    really done

--- instructor/backend/LocationsSolution/LocationsApi/Models/LocationsModels.cs
+++ instructor/backend/LocationsSolution/LocationsApi/Models/LocationsModels.cs
@@ -1,6 +1,23 @@
-﻿namespace LocationsApi.Models
+﻿namespace LocationsApi.Models;
+
+
+public record LocationsResponse
 {
-    public class LocationsModels
+    public List<LocationsResponse> _embedded { get; set; } = new();
+    public record LocationItemResponse
     {
+        public string Id { get; init; } = string.Empty;
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public string AddedBy { get; init; } = string.Empty;
+        public DateTime AddedOn { get; init; }
     }
 }
+/*  {
+    id: '1',
+    name: "Aladdin's Eatery",
+    description:
+      'On Mayfield, good lunch time - lots of options for vegetarians',
+    addedBy: 'Bob',
+    addedOn: '2023-01-01',
+  } */
