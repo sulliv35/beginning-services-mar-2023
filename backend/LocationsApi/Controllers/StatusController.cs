8df6a979b3cf2507b6d8efa1ad8a8b17c26a9f35
Mark Sullivan <sulliv35@miamioh.edu>
2023-03-28 09:32:13 -0400

    updating to latest

---
+++ backend/LocationsApi/Controllers/StatusController.cs
@@ -0,0 +1,41 @@
+﻿using LocationsApi.Models;
+using LocationsApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationsApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    private readonly UptimeClock _clock;
+
+    public StatusController(UptimeClock clock)
+    {
+        _clock = clock;
+    }
+
+
+    // GET /status
+    [HttpGet("/support")]
+    public async Task<ActionResult> GetStatus()
+    {
+        var sinceStartup =  DateTime.Now - _clock.UpSince;
+        var response = new GetStatusResponse()
+        {
+               ContactInfo = new ContactInfo()
+               {
+                   Name = "Bob Smith",
+                   Email = "bob@aol.com",
+                   Phone = "555-1212"
+               },
+               Uptime = new Uptime
+               {
+                   Hours = sinceStartup.Hours,
+                   Minutes = sinceStartup.Minutes,
+                   Days = sinceStartup.Days,
+                   Seconds = sinceStartup.Seconds,
+                   Milliseconds = sinceStartup.Milliseconds
+               }
+        };
+        return Ok(response);
+    }
+}
---
+++ backend/LocationsApi/Program.cs
@@ -0,0 +1,46 @@
+using LocationsApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// this wouldd have in "ConfigureServices" in your Startup.cs
+// Add services to the container.
+// all the behind the scenes stuff that makes up our application
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var clock = new UptimeClock();
+builder.Services.AddSingleton<UptimeClock>(clock);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(pol =>
+    {
+        pol.AllowAnyOrigin(); // Promiscuous 
+        pol.AllowAnyHeader();
+        pol.AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+// This would have been in the Configure method in startup
+// the thing that will actually handle incoming requests.
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+// Create the "Route Table" - phone book.
+// GET /status -> StatusController -> GetStatus
+
+
+app.Run(); // Blocking - where the "kestrel web server starts listening"
